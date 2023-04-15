ddbc24584e0f6425381634d3f9bac96444e567fe
Volodymyr Khymych <51357944+vovik541@users.noreply.github.com>
2023-04-15 14:13:46 +0200

    init commit

---
+++ src/test/java/com/microservice/librarian/LibrarianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.librarian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
